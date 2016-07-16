4b7d19dd3f078c0a759f2b2a1a461b52a67e7229
FaArIsH <faarishhassan@gmail.com>
2016-07-16 20:12:02 +0500

    Delete cm.mk

--- cm.mk
+++
@@ -1,8 +0,0 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-$(call inherit-product, device/samsung/lt02ltespr/full_lt02ltespr.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lt02ltespr TARGET_DEVICE=lt02lte
-
-PRODUCT_NAME := cm_lt02ltespr
