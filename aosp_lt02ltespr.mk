dac8bffddf9001e2b7cc251394143e60462f3895
FaArIsH <faarishhassan@gmail.com>
2016-07-17 21:38:01 +0500

    Update aosp_lt02ltespr.mk

--- aosp_lt02ltespr.mk
+++ aosp_lt02ltespr.mk
@@ -1,8 +1,8 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/lt02ltespr/full_lt02ltespr.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lt02ltespr TARGET_DEVICE=lt02lte
 
-PRODUCT_NAME := aosp_lt02ltespr
+PRODUCT_NAME := omni_lt02ltespr
