70ddc0a510d2c8edc9a00c8cbd97e2064e9b4811
FaArIsH <faarishhassan@gmail.com>
2016-07-17 11:40:10 +0500

    Update and rename cm.mk to aosp_lt02ltespr.mk

--- aosp_lt02ltespr.mk
+++ aosp_lt02ltespr.mk
@@ -5,4 +5,4 @@ $(call inherit-product, device/samsung/lt02ltespr/full_lt02ltespr.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lt02ltespr TARGET_DEVICE=lt02lte
 
-PRODUCT_NAME := cm_lt02ltespr
+PRODUCT_NAME := aosp_lt02ltespr
