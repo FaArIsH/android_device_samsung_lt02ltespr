fe66c7f31dc91f602a78e20a27be4798da8f00cc
FaArIsH <faarishhassan@gmail.com>
2016-07-18 18:58:55 +0500

    Update xosp.mk

--- xosp.mk
+++ xosp.mk
@@ -3,6 +3,9 @@ $(call inherit-product, vendor/xosp/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/lt02ltespr/full_lt02ltespr.mk)
 
+# Inherit some propietary XOSP stuff
+$(call inherit-product, vendor/xosp/config/xosp.mk)
+
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lt02ltespr TARGET_DEVICE=lt02lte
 
 PRODUCT_NAME := xosp_lt02ltespr
